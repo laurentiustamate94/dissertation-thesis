b37e3be41b3a64d210e7022bbaf85aa81d27b896
Laurentiu Stamate <laurentiu.stamate@outlook.com>
2019-04-19 00:36:08 +0200

    Add registration app project

---
+++ RegistrationApp/Controllers/HomeController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Fitbit.Api.Abstractions;
+using Fitbit.Api.Abstractions.Models.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PgpCore;
+using RegistrationApp.Models;
+
+namespace RegistrationApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IFitbitClient FitbitClient { get; }
+
+        public HomeController(IFitbitClient fitbitClient)
+        {
+            FitbitClient = fitbitClient;
+        }
+
+        public IActionResult Index()
+        {
+            var da = FitbitClient.Authentication.GetCodeGrantFlowWithPkceUrl(new PermissionsRequestType[]
+            {
+                PermissionsRequestType.Activity,
+                PermissionsRequestType.HeartRate,
+        PermissionsRequestType.Location,
+       PermissionsRequestType.Nutrition,
+         PermissionsRequestType.Profile,
+         PermissionsRequestType.Settings,
+         PermissionsRequestType.Sleep,
+         PermissionsRequestType.Social,
+         PermissionsRequestType.Weight
+            });
+
+            return Redirect(da);
+        }
+
+        public async Task<IActionResult> Privacy(string code)
+        {
+            await FitbitClient.Authentication.FinishCodeGrantFlowWithPkceAsync(code);
+
+            var da = await FitbitClient.HeartRate.GetHeartRateTimeSeriesAsync("", "");
+
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult MyClaims()
+        {
+            using (PGP pgp = new PGP())
+            {
+                // Generate keys
+                pgp.GenerateKey(@"C:\TEMP\keys\1.public.asc", @"C:\TEMP\keys\1.private.asc", "email@email.com", "password");
+                pgp.GenerateKey(@"C:\TEMP\keys\2.public.asc", @"C:\TEMP\keys\2.private.asc", "email@email.com", "password");
+                // Encrypt file
+                pgp.EncryptFile(@"C:\TEMP\keys\content.txt", @"C:\TEMP\keys\content__encrypted.pgp", @"C:\TEMP\keys\1.public.asc", true, true);
+                // Encrypt file with multiple keys
+                string[] publicKeys = Directory.GetFiles(@"C:\TEMP\keys\", "*.public.asc");
+                pgp.EncryptFile(@"C:\TEMP\keys\content.txt", @"C:\TEMP\keys\content__encrypted.pgp", publicKeys, true, true);
+                // Encrypt and sign file
+                pgp.EncryptFileAndSign(@"C:\TEMP\keys\content.txt", @"C:\TEMP\keys\content__encrypted_signed.pgp", @"C:\TEMP\keys\1.public.asc", @"C:\TEMP\keys\1.private.asc", "password", true, true);
+                // Decrypt file
+                pgp.DecryptFile(@"C:\TEMP\keys\content__encrypted.pgp", @"C:\TEMP\keys\content__decrypted.txt", @"C:\TEMP\keys\2.private.asc", "password");
+                // Decrypt signed file
+                pgp.DecryptFile(@"C:\TEMP\keys\content__encrypted_signed.pgp", @"C:\TEMP\keys\content__decrypted_signed.txt", @"C:\TEMP\keys\1.private.asc", "password");
+
+                // Encrypt stream
+                using (FileStream inputFileStream = new FileStream(@"C:\TEMP\keys\content.txt", FileMode.Open))
+                using (Stream outputFileStream = System.IO.File.Create(@"C:\TEMP\keys\content__encrypted2.pgp"))
+                using (Stream publicKeyStream = new FileStream(@"C:\TEMP\keys\1.public.asc", FileMode.Open))
+                    pgp.EncryptStream(inputFileStream, outputFileStream, publicKeyStream, true, true);
+
+                // Decrypt stream
+                using (FileStream inputFileStream = new FileStream(@"C:\TEMP\keys\content__encrypted2.pgp", FileMode.Open))
+                using (Stream outputFileStream = System.IO.File.Create(@"C:\TEMP\keys\content__decrypted2.txt"))
+                using (Stream privateKeyStream = new FileStream(@"C:\TEMP\keys\1.private.asc", FileMode.Open))
+                    pgp.DecryptStream(inputFileStream, outputFileStream, privateKeyStream, "password");
+            }
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RegistrationApp/Startup.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Fitbit.Api.Abstractions;
+using Fitbit.Api;
+
+namespace RegistrationApp
+{
+    public class Startup
+    {
+        public const string CookieScheme = "DissertationThesis";
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddAuthentication(CookieScheme) // Sets the default scheme to cookies
+               .AddCookie(CookieScheme, options =>
+               {
+                   options.AccessDeniedPath = "/account/denied";
+                   options.LoginPath = "/account/login";
+               });
+
+            services.AddSingleton<IFitbitClient>(new FitbitClient("__clientid__", "__clientsecret__", "https://localhost:44330/Home/Privacy", true));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
