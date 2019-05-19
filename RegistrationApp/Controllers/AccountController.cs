d9fe7153524598300544f6419915a1145da7e91b
Laurentiu Stamate <laurentiu.stamate@outlook.com>
2019-05-19 13:36:59 +0200

    Fully implement login and add disclaimers

--- RegistrationApp/Controllers/AccountController.cs
+++ RegistrationApp/Controllers/AccountController.cs
@@ -1,18 +1,20 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using RegistrationApp.Models;
 
 namespace RegistrationApp.Controllers
 {
     public class AccountController : Controller
     {
-       
+
         [HttpGet]
         public IActionResult Login(string returnUrl = null)
         {
@@ -27,16 +29,25 @@ namespace RegistrationApp.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Login(string userName, string password, string returnUrl = null)
+        public async Task<IActionResult> Login(string email, string password, bool rememberPassword, string returnUrl = null)
         {
             ViewData["ReturnUrl"] = returnUrl;
 
+            if (string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(password))
+            {
+                return View("Error", new ErrorViewModel
+                {
+                    Reason = "Email and password cannot be null",
+                    RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+                });
+            }
+
             // Normally Identity handles sign in, but you can do it directly
-            if (ValidateLogin(userName, password))
+            if (ValidateLogin(email, password))
             {
                 var claims = new List<Claim>
                 {
-                    new Claim("user", userName),
+                    new Claim("user", email),
                     new Claim("role", "Member")
                 };
 
@@ -52,6 +63,16 @@ namespace RegistrationApp.Controllers
                 }
             }
 
+            return View();
+        }
+
+        public IActionResult SignUp(string returnUrl = null)
+        {
+            return View();
+        }
+
+        public IActionResult ForgotPassword(string returnUrl = null)
+        {
             return View();
         }
 
--- RegistrationApp/Models/ErrorViewModel.cs
+++ RegistrationApp/Models/ErrorViewModel.cs
@@ -6,6 +6,10 @@ namespace RegistrationApp.Models
     {
         public string RequestId { get; set; }
 
+        public string Reason { get; set; }
+
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        public bool ShowReason => !string.IsNullOrEmpty(RequestId);
     }
 }
