2b37abcec0da68d0b09308083a1f2e6ec42cd0c8
Laurentiu Stamate <laurentiu.stamate@outlook.com>
2019-05-20 01:47:40 +0200

    Add periodic service for iPhone

---
+++ MobileApp/MobileApp.iOS/AppDelegate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+
+namespace MobileApp.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the 
+    // User Interface of the application, as well as listening (and optionally responding) to 
+    // application events from iOS.
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        //
+        // This method is invoked when the application has loaded and is ready to run. In this 
+        // method you should instantiate the window, load the UI into it and then make the window
+        // visible.
+        //
+        // You have 17 seconds to return from this method, or iOS will terminate your application.
+        //
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
+
+            UIApplication.SharedApplication.SetMinimumBackgroundFetchInterval(3);
+
+            return base.FinishedLaunching(app, options);
+        }
+
+        public override void PerformFetch(UIApplication application, Action<UIBackgroundFetchResult> completionHandler)
+        {
+            // Check for new data, and display it
+            MessagingCenter.Send<object, string>(this, "UpdateLabel", "Hello from iOS - " + new Random().Next(0, 99999));
+
+            // Inform system of fetch results
+            completionHandler(UIBackgroundFetchResult.NewData);
+        }
+    }
+}
