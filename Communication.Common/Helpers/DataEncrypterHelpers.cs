2251e099402c2c600529b3e069ef7b8983599c06
Laurentiu Stamate <laurentiu.stamate@outlook.com>
2019-05-30 00:36:06 +0200

    Add encryption helpers

---
+++ Communication.Common/Helpers/DataEncrypterHelpers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Communication.Common.Helpers
+{
+    public static class DataEncrypterHelpers
+    {
+        public static string ToBase64Data(this double number)
+        {
+            return Convert.ToBase64String(BitConverter.GetBytes(number));
+        }
+
+        public static string ToBase64Data(this string data)
+        {
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(data));
+        }
+    }
+}
