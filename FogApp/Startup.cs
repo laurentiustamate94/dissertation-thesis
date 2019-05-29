c5f4191f6d00f6296a2cf8712e0e43ab0dd44fa4
Laurentiu Stamate <laurentiu.stamate@outlook.com>
2019-05-30 01:29:40 +0200

    Use ForwardedHeaders when building with Release

--- FogApp/Startup.cs
+++ FogApp/Startup.cs
@@ -11,6 +11,7 @@ using FogApp.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -49,6 +50,12 @@ namespace FogApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+#if !DEBUG
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.All
+            });
+#endif
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
