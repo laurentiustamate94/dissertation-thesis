d063dd6758bc39f824a34e262b3c60b1e7bf8892
Laurentiu Stamate <laurentiu.stamate@outlook.com>
2019-05-30 00:37:48 +0200

    Add database code first

---
+++ CloudApp/DbModels/DissertationThesisContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CloudApp.DbModels
+{
+    public partial class DissertationThesisContext : DbContext
+    {
+        public DissertationThesisContext()
+        {
+        }
+
+        public DissertationThesisContext(DbContextOptions<DissertationThesisContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Users> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.3-servicing-35854");
+
+            modelBuilder.Entity<Users>(entity =>
+            {
+                entity.Property(e => e.Id)
+                    .HasMaxLength(65)
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.PasswordHash)
+                    .IsRequired()
+                    .HasMaxLength(256);
+
+                entity.Property(e => e.Role)
+                    .IsRequired()
+                    .HasMaxLength(256);
+
+                entity.Property(e => e.User)
+                    .IsRequired()
+                    .HasMaxLength(256);
+            });
+        }
+    }
+}
---
+++ CloudApp/DbModels/Users.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CloudApp.DbModels
+{
+    public partial class Users
+    {
+        public string Id { get; set; }
+        public string User { get; set; }
+        public string PasswordHash { get; set; }
+        public string Role { get; set; }
+    }
+}
