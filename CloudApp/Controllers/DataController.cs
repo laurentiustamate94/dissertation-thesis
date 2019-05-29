88018d91aaf84181cd43e0eb680184d701237972
Laurentiu Stamate <laurentiu.stamate@outlook.com>
2019-05-30 00:40:38 +0200

    Add Data controller for messages that come from FogApps

---
+++ CloudApp/Controllers/DataController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using CloudApp.Interfaces;
+using Communication.Common.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        public IDataPersistor DataPersistor { get; }
+
+        public DataController(IDataPersistor dataPersistor)
+        {
+            DataPersistor = dataPersistor;
+        }
+
+        [HttpPost]
+        public async Task<HttpResponseMessage> Post([FromBody] DataContract message)
+        {
+            return await DataPersistor.PersistData(message);
+        }
+
+        [HttpHead]
+        public HttpResponseMessage Head()
+        {
+            return new HttpResponseMessage(HttpStatusCode.Accepted);
+        }
+    }
+}
