049d0b87cf2574f39e72370c8880ba6e47a4c91d
Laurentiu Stamate <laurentiu.stamate@outlook.com>
2019-05-30 23:41:54 +0200

    Add details on how to setup a FogApp

--- CloudApp/DbModels/Users.cs
+++ CloudApp/DbModels/Users.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace CloudApp.DbModels
+﻿namespace CloudApp.DbModels
 {
     public partial class Users
     {
         public string Id { get; set; }
+
         public string User { get; set; }
+
         public string PasswordHash { get; set; }
+
         public string Role { get; set; }
     }
 }
